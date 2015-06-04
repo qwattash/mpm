ce8b9a7d112ab02bca5091399542b251cac18e0e
qwattash <mzz.lrd@gmail.com>
2015-06-04 22:40:16 +0200

    i) added implementation and test for parsing of the first page for each mod category

--- mpm/spiders/curseforge.py
+++ mpm/spiders/curseforge.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import urlparse
+from urllib import urlencode
+from urlparse import urljoin, urlparse, parse_qs, urlsplit, urlunsplit
 
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors.lxmlhtml import LxmlLinkExtractor
@@ -35,8 +36,54 @@ class CurseforgeSpider(CrawlSpider):
     rules = [
         Rule(LxmlLinkExtractor(restrict_xpaths="//ul[contains(@class, "\
                                "'listing-game-category')]//li/a"),
-                               callback="parse_mod")
+                               callback="parse_first_category")
     ]
+
+    def parse_first_category(self, response):
+        """
+        Extract paginated category mods and mod links
+        in the first category page
+        
+        Note: it is assumed that urls in the pagination are of the form
+        ``"/category?page=<integer>[&other]"``
+        """
+        mod_links = response.xpath("//ul[contains(@class, 'listing-project')]/li/div/a/@href")
+        for url in mod_links.extract():
+            full_url = urljoin(response.url, url)
+            yield Request(url=full_url, callback="parse_mod")
+
+        # try to guess the page range from urls in the pagination footer
+        paginator_urls = response.xpath("//ul[contains(@class, 'paging-list')]/li/a/@href")
+        # assume that the urls are 
+        page_numbers = []
+
+        # extract page numbers from the paginator in the footer
+        for url in paginator_urls.extract():
+            parsed = urlparse(url)
+            query = parse_qs(parsed.query)
+            if "page" in query:
+                page_numbers.append(int(query["page"][0]))
+
+        # url and query params used to build request objects
+        scheme, netloc, path, query, fragment = urlsplit(url)
+        base_page_query = parse_qs(query)
+
+        # page range to request
+        first_page = min(page_numbers)
+        last_page = max(page_numbers)
+        for page in range(first_page, last_page + 1):
+            base_page_query["page"] = page
+            query_string = urlencode(base_page_query)
+            query = query_string
+            url = urlunsplit((scheme, netloc, path, query, fragment))
+            url = urljoin(response.url, url)
+            yield Request(url=url, callback="parse_category")
+
+    def parse_category(self, response):
+        """
+        Extract urls in a mod category page
+        """
+        return None
 
     def parse_mod(self, response):
         """
--- tests/test_curseforge_spider.py
+++ tests/test_curseforge_spider.py
@@ -33,7 +33,7 @@ def test_curseforge_simplified_index_urls(response):
     spider = CurseforgeSpider()
     spider._follow_links = True
     parsed = iter(spider.parse(response))
-    urls = [urlparse.urljoin(response.url, url) for url in expected_urls]    
+    urls = [urlparse.urljoin(response.url, url) for url in expected_urls]
     assert_parse_requests(parsed, urls)
 
 @mock_scrapy_response("http://foo.org", "tests/resources/curseforge_mcmods_full.html")
@@ -45,5 +45,51 @@ def test_curseforge_full_index_urls(response):
     spider = CurseforgeSpider()
     spider._follow_links = True
     parsed = iter(spider.parse(response))
-    urls = [urlparse.urljoin(response.url, url) for url in expected_urls]    
+    urls = [urlparse.urljoin(response.url, url) for url in expected_urls]
+    assert_parse_requests(parsed, urls)
+
+@mock_scrapy_response("http://foo.org", "tests/resources/curseforge_cosmetic_base.html")
+def test_curseforge_simplified_mod_category(response):
+    """
+    :class:`CurseforgeSpider` url extraction from a simplified
+    curseforge category page.
+    Pagination is active and this extracts data from the first page,
+    some next-page urls are not rendered.
+    """
+    expected_urls = ["/mc-mods/223628-thaumcraft",
+                     "/mc-mods/225643-botania",
+                     "/mc-mods/221857-pams-harvestcraft",
+                     "/mc-mods/cosmetic?page=2",
+                     "/mc-mods/cosmetic?page=3",
+                     "/mc-mods/cosmetic?page=4",
+                     "/mc-mods/cosmetic?page=5",
+                     "/mc-mods/cosmetic?page=6",
+                     "/mc-mods/cosmetic?page=7"]
+    spider = CurseforgeSpider()
+    spider._follow_links = True
+    parsed = iter(spider.parse_first_category(response))
+    urls = [urlparse.urljoin(response.url, url) for url in expected_urls]
+    assert_parse_requests(parsed, urls)
+
+@mock_scrapy_response("http://foo.org", "tests/resources/curseforge_cosmetic_full.html")
+def test_curseforge_full_mod_category(response):
+    """
+    :class:`CurseforgeSpider` url extraction from the
+    curseforge category page with all the other content
+    Pagination is active and this extracts data from the first page,
+    some next-page urls are not rendered.
+    """
+    expected_urls = ["/mc-mods/223628-thaumcraft",
+                     "/mc-mods/225643-botania",
+                     "/mc-mods/221857-pams-harvestcraft",
+                     "/mc-mods/cosmetic?page=2",
+                     "/mc-mods/cosmetic?page=3",
+                     "/mc-mods/cosmetic?page=4",
+                     "/mc-mods/cosmetic?page=5",
+                     "/mc-mods/cosmetic?page=6",
+                     "/mc-mods/cosmetic?page=7"]
+    spider = CurseforgeSpider()
+    spider._follow_links = True
+    parsed = iter(spider.parse_first_category(response))
+    urls = [urlparse.urljoin(response.url, url) for url in expected_urls]
     assert_parse_requests(parsed, urls)
